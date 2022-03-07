d2bb7dfcbcfffec1f1b50da8a7842540cc623358
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-07 19:14:33 -0300

    add exercicio n3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/maior3num.js
@@ -0,0 +1,19 @@
+let a = 20;
+let b = 20;
+let c = 20;
+
+if (a > b && a > c) {
+    console.log("A é maior");
+}
+
+else if (b > a && b > c) {
+    console.log("B é maior");
+}
+
+else if (c > a && c > b) {
+    console.log("C é maior");
+}
+
+else {
+    console.log("Os valores são iguais");
+}

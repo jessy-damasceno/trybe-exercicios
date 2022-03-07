5a765df29ea1be6921725e05e38267e2ad3809a9
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-07 19:50:52 -0300

    add exercicio n5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/triangle.js
@@ -0,0 +1,13 @@
+let a = 120;
+    b = 30;
+    c = 30;
+
+if (a <= 0 || b <= 0 || c <= 0) {
+    console.log("false");
+
+} else if (a + b + c == 180) {
+    console.log("true");
+
+} else {
+    console.log("false");
+}

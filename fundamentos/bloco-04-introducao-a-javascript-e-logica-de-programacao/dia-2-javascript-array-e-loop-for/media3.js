7c7a1f31cad1289ec96b142f2222eb0568cc5e56
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-08 19:14:59 -0300

    add exercicio n3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/media3.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i+= 1) {
+    soma = soma + numbers[i];
+}
+
+let media = soma/numbers.length;
+
+console.log('Soma = ',soma);
+console.log('Média = ',media);

91fdfce5f47e38a8a89dc0e9d466d0d26e06851d
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-09 21:31:29 -0300

    change array1.js

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/array1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/array1.js
@@ -1,7 +1,20 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let i = 0; i < numbers.length; i+= 1){
-    console.log(numbers[i]);
-}
+// for (let i = 0; i < numbers.length; i+= 1){
+//     console.log(numbers[i]);
+// }
 
+let valorInicial = 1;
+let valorFinal = 125;
+let contador = 0;
 
+// for (let i = valorInicial; i < valorFinal; i+= 1)
+for (valorInicial; valorInicial <= valorFinal; valorInicial += 1 )
+    if (valorInicial % 3 === 0) {
+        contador += 1;
+        valorInicial += 1;      
+    } else {
+        valorInicial += 1;
+    }
+
+console.log(contador);

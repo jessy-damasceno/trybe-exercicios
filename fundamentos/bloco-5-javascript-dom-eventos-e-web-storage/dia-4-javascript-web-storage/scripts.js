5db601f476483910db1b74f76db8512ee16665bb
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-19 15:24:08 -0300

    fix sidebar event

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
@@ -2,6 +2,7 @@ const body = document.body;
 const sideBar = document.querySelector('.barra-lateral');
 const seta = document.querySelector('.seta');
 
+
 seta.addEventListener('click', () => {
-    sideBar.classList.toggle('close');
+    sideBar.classList.toggle('close'); 
 })

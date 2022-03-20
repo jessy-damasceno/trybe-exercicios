4bd5758029ebe73e2402487d2c41e7f3ed532717
Jessy Damasceno <jessy_bas@hotmail.com>
2022-03-19 21:08:56 -0300

    add changes

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
@@ -1,8 +1,11 @@
 const body = document.body;
 const sideBar = document.querySelector('.barra-lateral');
 const seta = document.querySelector('.seta');
+const icons = document.querySelector('.footIcons');
 
 
 seta.addEventListener('click', () => {
-    sideBar.classList.toggle('close'); 
+    sideBar.classList.toggle('close');
+    icons.classList.toggle('close');
 })
+

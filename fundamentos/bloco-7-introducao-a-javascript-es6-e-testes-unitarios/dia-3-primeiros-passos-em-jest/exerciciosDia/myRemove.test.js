e9bac38c8eced1698e3dce54298e923e17c13405
Jessy Damasceno <jessy_bas@hotmail.com>
2022-04-05 21:00:14 -0300

    add exercicio 2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exerciciosDia/myRemove.test.js
@@ -0,0 +1,15 @@
+const myRemove = require('./myRemove');
+
+describe('Teste da função myRemove', () => {
+  it('1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+  })
+
+  it('2. Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  it('3. Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  })
+})

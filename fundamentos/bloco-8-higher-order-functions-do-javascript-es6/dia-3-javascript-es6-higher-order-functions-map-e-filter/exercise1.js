58f67402a9d988d2508edbf736c2a4d71e8742cc
Jessy Damasceno <jessy_bas@hotmail.com>
2022-04-11 23:37:43 -0300

    add exercises 8.3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -71,4 +71,60 @@ const books1 = books.map((book) => {
   return `${bookName} - ${bookGenre} - ${bookAuthor}`;
 });
 
-console.log(books1);
+// console.log(books1);
+
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+function nameAndAge() {
+  const array = books.map((book) => ({
+    age: book.releaseYear - book.author.birthYear,
+    author: book.author.name,
+  }));
+
+  return array.sort((a, b) => a.age - b.age);
+}
+
+// console.log(nameAndAge());
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+const ficcao = books.filter(
+  (book) => book.genre === 'Ficção Científica' || book.genre === 'Fantasia'
+);
+
+// console.log(ficcao);
+
+// 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
+
+const sortOldBooks = books
+  .filter((book) => 2022 - book.releaseYear > 60)
+  .sort((a, b) => a.releaseYear - b.releaseYear);
+
+// console.log(sortOldBooks);
+
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+const sortedCienceFantasy = books
+  .filter(
+    (book) => book.genre.includes('Ficção') || book.genre.includes('Fant')
+  )
+  .map((book) => book.author.name)
+  .sort();
+
+// console.log(sortedCienceFantasy);
+
+// 6 - Crie um array com o nome de todos os livros com mais de 60 anos de publicação.
+
+const olderBookNames = books
+  .filter((book) => 2022 - book.releaseYear > 60)
+  .map((book) => book.name);
+
+// console.log(olderBookNames);
+
+// 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const authorWith3DotsOnName = books
+  .filter((book) => book.author.name.match(/[A-Z]\. [A-Z]\. [A-Z]\./g))
+  .map((book) => book.author.name);
+
+console.log(authorWith3DotsOnName);

90b4ba573bfdaef560dbbe91e0e6687420285a99
Jessy Damasceno <jessy_bas@hotmail.com>
2022-04-13 03:27:45 -0300

    add changes

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -1,16 +1,13 @@
 // 1 - Dada uma matriz, transforme em um array.
 
-const arrays = [
-  ['1', '2', '3'],
-  [true],
-  [4, 5, 6],
-];
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
 
-const flatten = () => arrays.reduce((novoArray, element) => {
-  element.forEach((arrayItem) => {
-    novoArray.push(arrayItem);
-  });
-  return novoArray;
-}, []);
+const flatten = () =>
+  arrays.reduce((novoArray, element) => {
+    element.forEach((arrayItem) => {
+      novoArray.push(arrayItem);
+    });
+    return novoArray;
+  }, []);
 
 console.log(flatten());

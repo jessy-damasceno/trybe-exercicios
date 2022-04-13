f8616379d7c562ac0f16dfe02e707221e74ad975
Jessy Damasceno <jessy_bas@hotmail.com>
2022-04-13 19:26:56 -0300

    add exercise 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -0,0 +1,19 @@
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const carros = [palio, shelbyCobra, chiron];
+
+// escreva toObject abaixo
+const toObject = (...args) => {
+  const carritos = [];
+  args.forEach(([modelo, marca, anoFabricacao]) =>
+    carritos.push({ modelo, marca, anoFabricacao })
+  );
+  return carritos;
+};
+
+console.log(toObject(...carros));
+console.log(toObject(palio));

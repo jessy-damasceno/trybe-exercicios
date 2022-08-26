0f49b3af2392a186e15cf5fa6d0472040eaa19a0
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-26 09:30:31 -0300

    pratica bmi

--- back-end/bloco-22-node/dia-1-node-runtime-assincrono/agora-a-pratica/bmi.js
+++ back-end/bloco-22-node/dia-1-node-runtime-assincrono/agora-a-pratica/bmi.js
@@ -1,3 +1,9 @@
-const bmi = (w, h) => w / (h ** 2);
+const readline = require('readline-sync');
+
+const bmi = () => {
+  const w = readline.questionInt('What\'s your weight? (kg) ');
+  const h = readline.questionInt('What\'s your height? (cm) ');
+  return (w / ((h / 100) ** 2)).toFixed(2);
+}
 
 console.log(bmi(70, 1.72));

f3356d2bdde42723eb3e789382546d560e38ec92
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-26 08:54:47 -0300

    22.1 exportando módulos - parte 2

--- back-end/bloco-22-node/dia-1-node-runtime-assincrono/index.js
+++ back-end/bloco-22-node/dia-1-node-runtime-assincrono/index.js
@@ -1,6 +1,11 @@
-const convert = require('./brlValue');
+const brlValue = require('./brlValue');
+
+console.log(brlValue);
+
+console.log(`Valor do dólar: ${brlValue.brl}`); // Valor do dólar: 5.37
+console.log(`10 dólares em reais: ${brlValue.usdToBrl(10)}`); // 10 dólares em reais: 53.7
 
 const usd = 10;
-const brl = convert(usd);
+const brl = brlValue.usdToBrl(usd);
 
 console.log(brl);

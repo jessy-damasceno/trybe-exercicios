aa26fd4c2e67bb558ad9e9d544f2ed82e49a9a26
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-26 08:41:56 -0300

    bloco 22

--- back-end/bloco-22-node/exercicios-dia/src/app.js
+++ back-end/bloco-22-node/exercicios-dia/src/app.js
@@ -1,5 +1,47 @@
 const express = require('express');
 
+const OK = 200;
+
 const app = express();
+
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const activity = activities.find(e => e.id === id);
+
+  res.status(OK).json(activity);
+});
+
+app.get('/myActivities', (_req, res) => {
+  res.status(OK).json({ activities });
+});
+
+app.get('/filter/myActivities', (req, res) => {
+  const { status } = req.query;
+
+  if (status) {
+    const response = activities.filter(e => e.status === status);
+    res.status(OK).json({ response });
+  } else {
+    res.status(404);
+  }
+})
 
 module.exports = app;

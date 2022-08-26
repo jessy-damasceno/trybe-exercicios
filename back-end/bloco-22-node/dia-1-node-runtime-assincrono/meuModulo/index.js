1eccf7ddc7c51514285420c4abaa502d32728a1a
Jessy Damasceno <jessy_bas@hotmail.com>
2022-08-26 09:00:15 -0300

    22.1 importando módulos

---
+++ back-end/bloco-22-node/dia-1-node-runtime-assincrono/meuModulo/index.js
@@ -0,0 +1,6 @@
+const funcionalidade1 = require('./funcionalidade-1');
+const funcionalidade2 = require('./funcionalidade-2');
+
+module.exports = { funcionalidade1, funcionalidade2 };
+
+// No exemplo acima, quando importarmos o módulo meuModulo, teremos um objeto contendo duas propriedades, que são as funcionalidades que exportamos dentro de meuModulo/index.js.
